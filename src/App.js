9eb2022193a8b7531f500504da771935f538cea8
usmangq12 <usmangq12@gmail.com>
2022-12-07 19:30:16 +0500

    Apply the loader , updated the theme,js file and removed the style in App.css

--- src/App.js
+++ src/App.js
@@ -1,45 +1,7 @@
-import { Grid } from "@mui/material";
-import { useEffect, useState } from "react";
 import "./App.css";
 import { Home } from "./pages/Home";
 
 function App() {
-  const [loading, setLoading] = useState(false);
-  useEffect(() => {
-    setLoading(true);
-    setTimeout(() => {
-      setLoading(false);
-    }, 1000);
-  }, []);
-  return (
-    <Grid container>
-      {loading ? (
-        <Grid
-          sx={{
-            width: "100%",
-            height: "100%",
-            position: "fixed",
-            top: 0,
-            left: 0,
-            display: "grid",
-            placeItems: "center",
-          }}
-        >
-          <Grid
-            sx={{
-              border: " 16px solid #e3e3e3",
-              borderTop: "16px solid #ecab03",
-              borderRadius: " 50%",
-              width: "70px",
-              height: "70px",
-              animation: "spin 1s linear infinite",
-            }}
-          ></Grid>
-        </Grid>
-      ) : (
-        <Home />
-      )}
-    </Grid>
-  );
+  return <Home />;
 }
 export default App;
--- src/theme.js
+++ src/theme.js
@@ -2,96 +2,32 @@ import { createTheme } from "@mui/material";
 import { Background } from "./assets/images/index.js";
 
 const theme = createTheme({
-  components: {
-    MuiCssBaseline: {
-      styleOverrides: {
-        body: {
-          background: `url(${Background})`,
-          padding: 0,
-          margin: 0,
-
-          //   "@media (prefers-reduced-motion: no-preference) ": {
-          //     "& .App-logo": {
-          //       animation: "App-logo-spin infinite 20s linear",
-          //     },
-          //   },
-          "& .demo": {
-            background:
-              "linear-gradient(1.87deg,rgba(255, 229, 0, 0.5) -1.18%,rgba(255, 230, 9, 0.435) 3.33%,rgba(255, 231, 23, 0.335) 10.55%,rgba(255, 233, 36, 0.245) 17.77%,rgba(255, 234, 47, 0.17) 25.89%,rgba(255, 234, 56, 0.11) 34.01%, rgba(255, 235, 62, 0.06) 42.13%,  rgba(255, 236, 67, 0.025) 51.15%,     rgba(255, 236, 70, 0.005) 60.17%,rgba(255, 236, 71, 0) 71.89% )",
-            clipPath: "polygon(0 0, 100% 0, 28% 139%, 77% 210%)",
-            width: " 317px",
-            height: "800px",
-            position: "absolute",
-            top: "-50%",
-            left: " 30%",
-            padding: "1rem",
-          },
-          "@keyframes spin": {
-            "0%": {
-              transform: " rotate(0deg)",
-            },
-
-            "100%": {
-              transform: " rotate(360deg)",
-            },
-          },
-          " @keyframes App-logo-spin": {
-            from: {
-              transform: " rotate(0deg)",
-            },
-            to: {
-              transform: "rotate(360deg)",
-            },
-          },
-
-          "@keyframes rotation": {
-            from: {
-              WebkitTransform: "rotate(0deg)",
-            },
-            to: {
-              WebkitTransform: "rotate(-359deg)",
-            },
-          },
-          "@keyframes rotate ": {
-            from: {
-              WebkitTransform: "rotate(-180deg)",
-            },
-            to: {
-              WebkitTransform: "rotate(-549deg)",
-            },
-          },
-          "@keyframes coins": {
-            "50%": {
-              transform: "translateY(30px)",
-            },
-          },
-          "@keyframes coin": {
-            "  from": {
-              transform: "translateY(160px)",
-            },
-            to: {
-              transform: "translateY(0px)",
-            },
-          },
+    components: {
+        MuiCssBaseline:{
+            styleOverrides:{
+                body: {
+                    background: `url(${Background})`,
+                    padding:0,
+                    margin:0
+                  },
+            }
         },
-      },
-    },
-    MuiTypography: {
-      styleOverrides: {
-        root: {
-          color: "#fff",
+        MuiTypography:{
+            styleOverrides:{
+                root:{
+                    color:"#fff"
+                }
+            }
         },
-      },
-    },
-    MuiListItem: {
-      styleOverrides: {
-        root: {
-          display: "list-item",
-          paddingTop: 0,
-          paddingBottom: 0,
-        },
-      },
-    },
-  },
+        MuiListItem:{
+            styleOverrides:{
+                root:{
+                    display:'list-item',
+                    paddingTop:0,
+                    paddingBottom:0
+                }
+            }
+        }
+    }
 });
 export default theme;
